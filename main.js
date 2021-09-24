f1282022da345ad1d0d9abee27420cdd50d481d8
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-09-24 23:12:03 +0530

    v-model textarea input

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const App = {
+  data() {
+    return {
+      message: "",
+    };
+  },
+};
+
+Vue.createApp(App).mount('#app')
