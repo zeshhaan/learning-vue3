97e10928338a7f7c1c989edf1a2ed25ada4d5fb9
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-09-26 15:39:14 +0530

    methods showcasing counter

--- main.js
+++ main.js
@@ -4,9 +4,21 @@ const App = {
   data() {
     return {
       dish: "",
-      num1:1,
-      num2:2
+      num1: 1,
+      num2: 2,
+      count: 0,
+      disabled: 'disabled'
     };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      if (this.count != 0) {
+        this.count--;
+      }
+    },
   },
 };
 
