0555c833a9ed40b6c0ca5b9f807d2f420552a54c
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-09-26 22:05:17 +0530

    improved button text, restrict user to add only one item from the button click

--- main.js
+++ main.js
@@ -10,37 +10,41 @@ const App = {
           name: "Porotta",
           url: "https://www.whiskaffair.com/wp-content/uploads/2020/04/Kerala-Parotta-3.jpg",
           price: 10,
+          count: 0,
         },
         {
           id: 2,
           name: "BeefFry",
           url: "https://i1.wp.com/www.sujiscooking.com/wp-content/uploads/2018/12/Kerala-Beef-Roast-or-Beef-Ularthiyathu-1.jpg?fit=1100%2C1650&ssl=1",
           price: 115,
+          count: 0,
         },
         {
           id: 3,
           name: "Unnakayi",
           url: "http://1.bp.blogspot.com/-YCkzE3Q6r5I/Vf6X8phZziI/AAAAAAAADsg/2TuEHOJpufo/s1600/IMG_5277.jpg",
           price: 15,
+          count: 0,
         },
         {
           id: 4,
           name: "ButterChicken",
           url: "https://media.istockphoto.com/photos/tasty-butter-chicken-curry-dish-from-indian-cuisine-picture-id1300632831?b=1&k=20&m=1300632831&s=170667a&w=0&h=FjWbieLh-NkmRiL4xsaa3nEMjYmXxmRYpBtvipKGNW8=",
           price: 120,
+          count: 0,
         },
       ],
       price: 0,
-      count: 0,
     };
   },
   methods: {
     addToCart(e) {
       console.table(e);
-      const { id, name, url, price } = e;
-      this.cart.push({ name, id, url, price });
+      let { id, name, url, price, count } = e;
+      count++;
+      this.cart.push({ name, id, url, price, count });
       this.price = this.price + price;
-      this.count = this.cart.length;
+      // this.count = this.cart.length;
     },
   },
 };
