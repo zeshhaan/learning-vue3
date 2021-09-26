20e76e423db19de7ca73f0b0d4c3fe65b70a4187
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-09-26 18:01:15 +0530

    improvements - makes use of methods + some ui improvements

--- main.js
+++ main.js
@@ -3,30 +3,41 @@ import "./style.css";
 const App = {
   data() {
     return {
-      checkedDish: [],
+      cart: [],
       dishes: [
         {
           id: 1,
           name: "Porotta",
-          url: "https://media.istockphoto.com/photos/parotta-or-porotta-is-a-layered-flatbread-made-from-maida-or-atta-picture-id1339811566?b=1&k=20&m=1339811566&s=170667a&w=0&h=3vddMomSnKEkBL6giYshEPk4-nXCBjuunoJp8-m2lNI=",
+          url: "https://www.whiskaffair.com/wp-content/uploads/2020/04/Kerala-Parotta-3.jpg",
+          price: 4,
         },
         {
           id: 2,
           name: "BeefFry",
           url: "https://i1.wp.com/www.sujiscooking.com/wp-content/uploads/2018/12/Kerala-Beef-Roast-or-Beef-Ularthiyathu-1.jpg?fit=1100%2C1650&ssl=1",
+          price: 25,
         },
         {
           id: 3,
-          name: "Chappathi",
-          url: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Chapati-Breads_EXPS_FT20_136906_F_0721_1.jpg",
+          name: "Unnakayi",
+          url: "http://1.bp.blogspot.com/-YCkzE3Q6r5I/Vf6X8phZziI/AAAAAAAADsg/2TuEHOJpufo/s1600/IMG_5277.jpg",
+          price: 4,
         },
         {
           id: 4,
           name: "ButterChicken",
           url: "https://media.istockphoto.com/photos/tasty-butter-chicken-curry-dish-from-indian-cuisine-picture-id1300632831?b=1&k=20&m=1300632831&s=170667a&w=0&h=FjWbieLh-NkmRiL4xsaa3nEMjYmXxmRYpBtvipKGNW8=",
+          price: 22,
         },
       ],
     };
+  },
+  methods: {
+    addToCart(e) {
+      console.table(e);
+      const { id, name, url, price } = e;
+      this.cart.push({ name, id, url, price });
+    },
   },
 };
 
