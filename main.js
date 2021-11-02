a16fada4cd478a6a7733ade150c920fd71e96332
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-11-02 23:20:19 +0530

    feat: computed properties example

--- main.js
+++ main.js
@@ -1,26 +1,89 @@
-import "./style.css";
+import "./style.scss";
 
 const App = {
   data() {
     return {
-      name: "",
-      email: "",
-      message: "",
-      activeClass: "active",
-      submitText: "Submit",
+      selected: "",
+      labels: ["science", "math", "poetry", "history"],
+      posts: [
+        {
+          author: "@vFitzgerald",
+          title: "Quod Ducimus Omnis",
+          label: "science",
+        },
+        {
+          author: "@mPalmer",
+          title: "Vero Eius Laboriosam Ex Repudiandae",
+          label: "math",
+        },
+        {
+          author: "@mDean",
+          title: "Magnam Odit",
+          label: "science",
+        },
+        {
+          author: "@tCole",
+          title: "Velit Mollitia Voluptates Assumenda",
+          label: "science",
+        },
+        {
+          author: "@jCooper",
+          title: "Eum Commodi Cupiditate",
+          label: "poetry",
+        },
+        {
+          author: "@lLamb",
+          title: "Amet",
+          label: "history",
+        },
+        {
+          author: "@fMartin",
+          title: "Voluptatem Fuga Cum Asperiores Error",
+          label: "science",
+        },
+        {
+          author: "@eHayes",
+          title: "Ipsa Consectetur Rerum Repellat Quia",
+          label: "math",
+        },
+        {
+          author: "@cCollier",
+          title: "Dolor Nihil Delectus",
+          label: "science",
+        },
+        {
+          author: "@cBenson",
+          title: "Labore Ipsum Ad Pariatur",
+          label: "poetry",
+        },
+      ],
+      newTitle: "",
+      newAuthor: "",
+      newLabel: "",
     };
   },
   methods: {
-    submitForm() {
-      this.submitText = "Submitting...";
-      setTimeout(() => {
-        this.submitText = "Sent";
-      }, 1000);
+    addNewPost() {
+      let newPost = {
+        author: this.newAuthor,
+        title: this.newTitle,
+        label: this.newLabel,
+      };
+      this.posts.push(newPost);
+      this.newAuthor = "";
+      this.newLabel = "";
+      this.newTitle = "";
     },
   },
   computed: {
-    response() {
-      return `Thanks for contacting ${this.name}, you will hear back soon on your email ${this.email} for your request about "${this.message}"`;
+    filteredLabels(e) {
+      let filter = new RegExp(this.selected, "i");
+      return this.posts.filter((el) => el.label.match(filter));
+    },
+  },
+  watch: {
+    selected(newVal, oldVal) {
+      console.log(`the label is changed from ${oldVal} to ${newVal}`);
     },
   },
 };
