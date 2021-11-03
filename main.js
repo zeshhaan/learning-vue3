f66e326e6f1ab42444631e29a5523afa6f3b68cb
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-11-03 10:53:27 +0530

    feat: watchers ⚡️

--- main.js
+++ main.js
@@ -3,87 +3,22 @@ import "./style.scss";
 const App = {
   data() {
     return {
-      selected: "",
-      labels: ["science", "math", "poetry", "history"],
-      posts: [
-        {
-          author: "@vFitzgerald",
-          title: "Quod Ducimus Omnis",
-          label: "science",
-        },
-        {
-          author: "@mPalmer",
-          title: "Vero Eius Laboriosam Ex Repudiandae",
-          label: "math",
-        },
-        {
-          author: "@mDean",
-          title: "Magnam Odit",
-          label: "science",
-        },
-        {
-          author: "@tCole",
-          title: "Velit Mollitia Voluptates Assumenda",
-          label: "science",
-        },
-        {
-          author: "@jCooper",
-          title: "Eum Commodi Cupiditate",
-          label: "poetry",
-        },
-        {
-          author: "@lLamb",
-          title: "Amet",
-          label: "history",
-        },
-        {
-          author: "@fMartin",
-          title: "Voluptatem Fuga Cum Asperiores Error",
-          label: "science",
-        },
-        {
-          author: "@eHayes",
-          title: "Ipsa Consectetur Rerum Repellat Quia",
-          label: "math",
-        },
-        {
-          author: "@cCollier",
-          title: "Dolor Nihil Delectus",
-          label: "science",
-        },
-        {
-          author: "@cBenson",
-          title: "Labore Ipsum Ad Pariatur",
-          label: "poetry",
-        },
-      ],
-      newTitle: "",
-      newAuthor: "",
-      newLabel: "",
+      orderStatus: "Where is my taco...",
+      orderSubmitted: null,
     };
   },
   methods: {
-    addNewPost() {
-      let newPost = {
-        author: this.newAuthor,
-        title: this.newTitle,
-        label: this.newLabel,
-      };
-      this.posts.push(newPost);
-      this.newAuthor = "";
-      this.newLabel = "";
-      this.newTitle = "";
-    },
-  },
-  computed: {
-    filteredLabels(e) {
-      let filter = new RegExp(this.selected, "i");
-      return this.posts.filter((el) => el.label.match(filter));
+    getTaco() {
+      this.orderStatus = "🌮!";
     },
   },
   watch: {
-    selected(newVal, oldVal) {
-      console.log(`the label is changed from ${oldVal} to ${newVal}`);
+    orderStatus() {
+      //add code here to run when orderStatus is changed
+      this.orderSubmitted = "Your order was submitted";
+      setTimeout(() => {
+        this.orderSubmitted = null;
+      }, 1000);
     },
   },
 };
