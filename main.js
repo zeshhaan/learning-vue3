d9b527c769f04f8ae95de5887170790cb6427ab3
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-11-07 19:44:37 +0530

    feat: local components

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
 import "./style.scss";
 
-const app = Vue.createApp({})
-  .component("plan-picker", {
-    template: "#plan-picker-template",
-    data() {
-      return {
-        plans: ["The Addict", "The Curious", "The Single"],
-      };
+const PlanComponent = {
+  template: "#plan-template",
+  props: {
+    name: {
+      type: String,
+      default: "Zeeshan",
+      required: true,
     },
-  })
+  },
+  data() {
+    return {
+      name1: "aaa",
+    };
+  },
+};
 
-  .component("plan", {
-    template: "#plan-template",
-    props: {
-      name: {
-        type: String,
-        default: "Zeeshan",
-        required: true,
-      },
-    },
-    data() {
-      return {
-        name1: "aaa",
-      };
-    },
-  })
-  .mount("#app");
+const PlanPickerComponent = {
+  components: { plan: PlanComponent },
+  template: "#plan-picker-template",
+  data() {
+    return {
+      plans: ["The Addict", "The Curious", "The Single"],
+    };
+  },
+};
+
+const app = Vue.createApp({
+  components: {
+    "plan-picker": PlanPickerComponent,
+  },
+})
+.mount("#app");
