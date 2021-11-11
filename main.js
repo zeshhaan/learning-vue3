6fa4183bf16896f3e84b00ab69f5b05de4694509
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-11-11 07:34:00 +0530

    🥉 add items to cart. feat: emitting props 2 steps up ⚡️ and then passing down using provide/inject.

--- main.js
+++ main.js
@@ -1,63 +1,94 @@
 import "./style.scss";
 
-const App = {
+const app = Vue.createApp({
   data() {
     return {
       cart: [],
-      dishes: [
-        {
-          id: 1,
-          name: "Porotta",
-          url: "https://www.whiskaffair.com/wp-content/uploads/2020/04/Kerala-Parotta-3.jpg",
-          price: 10,
-          count: 0,
-        },
-        {
-          id: 2,
-          name: "BeefFry",
-          url: "https://i1.wp.com/www.sujiscooking.com/wp-content/uploads/2018/12/Kerala-Beef-Roast-or-Beef-Ularthiyathu-1.jpg?fit=1100%2C1650&ssl=1",
-          price: 115,
-          count: 0,
-        },
-        {
-          id: 3,
-          name: "Unnakayi",
-          url: "http://1.bp.blogspot.com/-YCkzE3Q6r5I/Vf6X8phZziI/AAAAAAAADsg/2TuEHOJpufo/s1600/IMG_5277.jpg",
-          price: 15,
-          count: 0,
-        },
-        {
-          id: 4,
-          name: "ButterChicken",
-          url: "https://media.istockphoto.com/photos/tasty-butter-chicken-curry-dish-from-indian-cuisine-picture-id1300632831?b=1&k=20&m=1300632831&s=170667a&w=0&h=FjWbieLh-NkmRiL4xsaa3nEMjYmXxmRYpBtvipKGNW8=",
-          count: 0,
-          price: 120,
-        },
-      ],
     };
   },
-
   methods: {
     addToCart(e) {
+      console.log(e);
       let { id, name, url, price, count } = e;
       count++;
       this.cart.push({ name, id, url, price, count });
     },
-
-    removeItem(e) {
-      // first find index and then insert that index into array.splice
-      this.cart.splice(
-        this.cart.findIndex(({ name }) => name === e.name),
-        1
-      );
-    },
   },
-
-  computed: {
-    totalPrice() {
-      return this.cart.reduce((total, obj) => obj.sum + total, 0);
-    },
+  provide() {
+    return {
+      cart: this.cart,
+    };
   },
-};
+})
+  .component("dishes", {
+    template: "#dishes-template",
+    props: ["cart-items"],
 
-Vue.createApp(App).mount("#app");
+    data() {
+      return {
+        // cart: [],
+        dishes: [
+          {
+            id: 1,
+            name: "Porotta",
+            url: "https://www.whiskaffair.com/wp-content/uploads/2020/04/Kerala-Parotta-3.jpg",
+            price: 10,
+            count: 0,
+          },
+          {
+            id: 2,
+            name: "Beef Fry",
+            url: "https://i1.wp.com/www.sujiscooking.com/wp-content/uploads/2018/12/Kerala-Beef-Roast-or-Beef-Ularthiyathu-1.jpg?fit=1100%2C1650&ssl=1",
+            price: 115,
+            count: 0,
+          },
+          {
+            id: 3,
+            name: "Unnakayi",
+            url: "http://1.bp.blogspot.com/-YCkzE3Q6r5I/Vf6X8phZziI/AAAAAAAADsg/2TuEHOJpufo/s1600/IMG_5277.jpg",
+            price: 15,
+            count: 0,
+          },
+          {
+            id: 4,
+            name: "Butter Chicken",
+            url: "https://media.istockphoto.com/photos/tasty-butter-chicken-curry-dish-from-indian-cuisine-picture-id1300632831?b=1&k=20&m=1300632831&s=170667a&w=0&h=FjWbieLh-NkmRiL4xsaa3nEMjYmXxmRYpBtvipKGNW8=",
+            count: 0,
+            price: 120,
+          },
+        ],
+      };
+    },
+    // methods: {
+    //   addToCart(e) {
+    //     let { id, name, url, price, count } = e;
+    //     count++;
+    //     this.cart.push({ name, id, url, price, count });
+    //   },
+    // },
+  })
+  .component("dish-item", {
+    template: "#dish-item-template",
+    props: ["dish"],
+  })
+  .component("cart", {
+    template: "#cart-template",
+    inject: ["cart"],
+    data() {
+      return {
+        cart: this.cart,
+      };
+    },
+    // methods: {
+    //   addToCart(e) {
+    //     let { id, name, url, price, count } = e;
+    //     count++;
+    //     this.cart.push({ name, id, url, price, count });
+    //   },
+    // },
+  })
+  .component("cart-item", {
+    template: "#cart-item-template",
+    props: ["item", "cart"],
+  })
+  .mount("#app");
